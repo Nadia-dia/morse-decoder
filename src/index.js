5855200869d1b8c4622a63446f9b90c2c60b86aa
Nadzeya Kaliada <hopenadya88@gmail.com>
2024-10-26 18:59:59 +0200

    feat: implement decode function

--- src/index.js
+++ src/index.js
@@ -38,9 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let words = expr.split("**********");
+    console.log(words);
+    let result = '';
+
+    for(let word of words){
+        let morseCode = ''; // coded sign from morse to binary
+        let decoded = '';
+
+        for(let i = word.length - 1; i >= 0; i -= 2){
+            let sign = `${word[i - 1]}${word[i]}`; // for reading every sign separately
+
+            if(sign ==='00' || ((i - 1) % 10) === 0) { // if only 00 left or reached the start of sign's binary code
+                if(sign === '10') morseCode = '.' + morseCode;
+                else if(sign === '11') morseCode = '-' + morseCode;
+                
+                decoded = decodeMorseToLatin(morseCode) + decoded;
+                i = (Math.floor(i / 10) * 10 - 1) + 2; // end position of the next coded sign plus 2
+                morseCode = '';
+            }
+            else if(sign === '10') morseCode = '.' + morseCode;
+            else if(sign === '11') morseCode = '-' + morseCode;
+        }
+        result += ' ' + decoded;
+    }
+    return result.trim();
+}
+
+//Function decoding sign in Morse Code to Latin
+function decodeMorseToLatin(sign){
+    for(let key in MORSE_TABLE){
+        if(key === sign){
+            return MORSE_TABLE[key];
+        }
+    }
+    return -1;
 }
 
 module.exports = {
-    decode
+  decode
 }
